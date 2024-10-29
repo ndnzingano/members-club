217670b4cd3c2813d1cc5b55ded6f05df6ad2de4
nadine.zingano <nadine.zingano@mitzit.com.br>
2024-10-29 10:47:23 -0300

    add user data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let idCardForm = document.getElementById('id-card-form');
 let idCardNumber = document.getElementById('id-card-input');
 let formButton = document.getElementById('id-card-form-button');
 let username = document.getElementById('name');
+let clientSince = document.getElementById('client-since');
 
 idCardNumber.addEventListener('input', function (event) {
   if (!event.target.value) {
@@ -17,9 +18,10 @@ idCardForm.addEventListener('submit', function (event) {
   fetch(`http://localhost:3001/clients/${idCardNumber.value}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log('data', data.name);
+      console.log('data', data);
       if (data) {
         username.innerText = data.name ?? '';
+        clientSince.innerText = `Cliente desde ${data.clientSince}` ?? '';
       }
     });
 });
