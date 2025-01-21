6f34e65df0a66d02c9ce4190d4c777957f87ac40
nadine.zingano <nadine.zingano@mitzit.com.br>
2025-01-21 15:35:10 -0300

    chore: page layout

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ let idCardNumber = document.getElementById('id-card-input');
 let formButton = document.getElementById('id-card-form-button');
 let username = document.getElementById('name');
 let clientSince = document.getElementById('client-since');
+let clientId = document.getElementById('card-id');
+const loyaltyCards = document.querySelectorAll('.loyalty-card');
+let historyHeaderAmount = document.getElementById('history-header-amount');
+let historyList = document.getElementById('history-list');
+let progressRemaining = document.getElementById('progress-remaining');
 
 idCardNumber.addEventListener('input', function (event) {
   if (!event.target.value) {
@@ -12,16 +17,50 @@ idCardNumber.addEventListener('input', function (event) {
   }
 });
 
+function updateProgress(current, total) {
+  const progressBar = document.getElementById('progress-bar');
+  const progressText = document.getElementById('progress-text');
+  const percentage = (current / total) * 100;
+
+  progressBar.style.width = percentage + '%';
+  progressText.textContent = `${current} de ${total}`;
+}
+
 idCardForm.addEventListener('submit', function (event) {
   event.preventDefault();
 
   fetch(`http://localhost:3001/clients/${idCardNumber.value}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log('data', data);
       if (data) {
         username.innerText = data.name ?? '';
         clientSince.innerText = `Cliente desde ${data.clientSince}` ?? '';
+        clientId.innerText = `ID: ${data.id}` ?? '';
+
+        loyaltyCards.forEach((card, index) => {
+          if (index <= data.loyaltyCard.totalCuts) {
+            card.innerHTML = `<img src="assets/PinCheck.svg"  class="pincheck" />`;
+          }
+        });
+        historyHeaderAmount.innerText = `${data.appointmentHistory.length} cortes`;
+
+        data.appointmentHistory.forEach((history) => {
+          console.log('history', history);
+          const historyItem = document.createElement('div');
+          historyItem.classList.add('history-item');
+          historyItem.innerHTML = `          
+            <div>
+              <p>${history.date}</p>
+              <p>${history.time}</p>
+            </div>
+            <img src="assets/PinCheckFilled.svg" /> 
+          `;
+
+          historyList.appendChild(historyItem);
+        });
+
+        progressRemaining.innerHTML = `<span>${data.loyaltyCard.cutsRemaining}</span> cortes restantes`;
+        updateProgress(1, 10);
       }
     });
 });
